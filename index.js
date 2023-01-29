7a7e84faeb6153bc1c0f8f4e0467680a720ad22f
Gaurav <gauravkshah312@gmail.com>
2023-01-29 14:14:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+document.getElementById('getApi').addEventListener('click', getApi);
+document.getElementById('getApi').addEventListener('onkeydown', getApi);
+const apiId = 'e2d2fe2c76f848a2b1570500222607';
+
+function getApi() {
+
+  ip = encodeURI(document.getElementById('ipAddress').value);
+  console.log("IP address :", ip);
+
+  fetch(`https://get.geojs.io/v1/ip/country.json?ip=${ip}`, {
+    method: 'GET',
+  })
+    .then(function (data) {
+      return data.json();
+    })
+    .then((data) => {
+      let output = `
+            <div>
+                <div style="color:black; width:10rem; background-color:white; border-radius: 10px;" class="p-3">        
+                    <p class="m-0" style="font-width:bold; font-size:2rem;">${data[0].country}</p>
+                    <p class="m-0" style="font-width:bold; font-size:2rem;"> ${data[0].name.toUpperCase()}</sup></p>
+                </div>
+            </div>
+            `;
+      console.log('printing data', data);
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch((err) => console.log(err))
+}
